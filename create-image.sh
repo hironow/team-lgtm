e1ad418e761b70765b204f3a4ffd7d3a22f0aa48
hironow <hironow365@gmail.com>
2019-04-30 19:53:09 +0900

    update docker

--- create-image.sh
+++ create-image.sh
@@ -1,1 +1,2 @@
+docker build . --file ./backend/Dockerfile --tag gcr.io/team-lgtm-dev/backend
 docker build . --file ./frontend/Dockerfile --tag gcr.io/team-lgtm-dev/frontend
--- run-image.sh
+++ run-image.sh
@@ -1,1 +1,2 @@
-docker run -p 80:8080 gcr.io/team-lgtm-dev/frontend
+docker run -p 80:8080 gcr.io/team-lgtm-dev/backend
+# docker run -p 80:8080 gcr.io/team-lgtm-dev/frontend
